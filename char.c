9052f4e69a8dc8aaa0cf7fe185f587ad5dff71b5
uche-p <uchepreciousokekeani>
2022-09-26 06:22:28 +0100

    i tried making a little prediction game.

---
+++ char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	char b2[];
+	printf("enter your name:\n");
+	scanf("%s\n", &b2);
+
+
+int ch1 = 25;
+	char ch2 = 'b';
+printf("welcome %s\n", b2);
+	printf("your lucky number is  %d\n", ch1);
+	printf("and your lucky alphabet is %c\n", ch2);
+	printf("Your birthstone is ruby\n");
+	printf("your love interest will come to you on 12\n");
+	return 0;
+}
